f465df11207e23188b50144b81ce63234446cb5e
Salsa-Dude <arias.joseph.a@gmail.com>
2019-10-23 17:28:10 -0400

    added css lecture

---
+++ 23-css/README.md
@@ -0,0 +1,5 @@
+
+What does CSS Stand for?
+Casading style sheets
+
+
---
+++ 23-css/app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    
+    def home
+        
+    end
+
+    def grand_canyon
+
+    end
+
+    def photo_gallery
+
+    end
+end
