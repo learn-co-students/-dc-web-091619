e90c0256f9888466e7a0b777fa0f916a7de17308
Ann <thuyanannduong@gmail.com>
2019-10-28 12:05:20 -0400

    add 24

---
+++ 24-JS-Intro/README.md
@@ -0,0 +1,39 @@
+## Intro to Javascript
+- Invented to compete with native Desktop applications
+- HTML, CSS, and JS (behavior)
+- The index.html file MUST contain a script tag reference to the script.js file
+
+### Learning a New Programming language
+- How to Debug (
+  console.log(), alert(), debugger
+  )
+- Documentation [MDN]((https://developer.mozilla.org/en-US/))
+
+### Difference between Ruby and JS
+- lots of diff ways to declare variables (var, let, const)
+- sometimes more explicit (iterators, with your return values)
+- sometimes you don't have to be as explicit
+- type coercion
+
+### Datatypes
+
+Primitive Data Types
+- Boolean (true or false; but everything has a truthy or falsey)
+- Numbers (be careful of floating points)
+- String (concatenation, interpolation)
+- null
+- undefined
+- Symbols technically (not used though)
+
+Objects
+- object
+- arrays
+- functions 
+
+
+### Pass by Value versus Pass by Reference
+
+
+### Functions as Objects
+- all the difference ways to write functions
+- hoisting
---
+++ 24-JS-Intro/script.js
@@ -0,0 +1,112 @@
+console.log("connected")
+
+// function test(a){
+//   let value = a
+//   console.log(typeof value)
+// }
+
+// let a = 6
+// let b = 3
+// let c = a + b
+
+
+//undefined is when you have a variable,
+//but no value(data) yet
+// let b
+
+//null is when you have a variable,
+//but it does have a value
+// const c = null
+// c = "no null"
+
+// var a = "a" // you can redeclare
+// var a = 1
+
+// let a = "a" //you can reassigned
+// a = 1
+
+// const a = "a" //you can reassigned
+// a = 1
+
+
+
+// var obj = {}
+
+// var arr = []
+
+// class Something {
+//
+//
+// }
+
+//PASS by value to primitive
+// function double(x){
+//   let y = 2 * x
+//   return y
+// }
+//
+// let a = 4
+// let b = double(a)
+//
+// console.log("The value of a: ", a)
+// console.log("The value of b: ", b)
+
+
+// function double(x){
+//   let y = x
+//   y.num = y.num * 2
+//   return y
+// }
+//
+// let a = {num: 4}
+// let b = double(a)
+//
+// console.log("The value of a: ", a)
+// console.log("The value of b: ", b)
+
+
+
+//function the they many ways to write them
+
+//declaration
+function double(num){
+  return num * 2
+}
+//
+// //expression
+let timesTwo = function(num){
+  return num * 2
+};
+//
+// //arrow function
+let multipleByTwo = num => {
+  return num * 2
+}
+//
+// function test(){
+//   return 1 + 1
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
